2820444452daac5a133b3f4752a86717a5e2fe39
Karl Artur Lokk <46595806+Paarthunax@users.noreply.github.com>
2022-05-18 23:21:38 +0300

    s

--- src/main/java/com/example/pjkt/Main.java
+++ src/main/java/com/example/pjkt/Main.java
@@ -3,6 +3,7 @@ package com.example.pjkt;
 import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.SubScene;
 import javafx.scene.control.Button;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Ellipse;
@@ -11,53 +12,61 @@ import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
 import java.util.List;
+import java.util.Scanner;
 
 public class Main extends Application{
+
+    public static Group groupTaimed;
+    public static Group groupTaust;
+    private static String[] taimeSordid = new String[] {"hernes", "tomat", "kurk", "petersell", "uba"};
+
     public static void main(String[] args){
         launch();
     }
 
     public void start(Stage primaryStage) throws Exception {
         Peenar peenar = new Peenar(100,100,0);
-        Taim kurk = new Taim("Kurk",7);
-        Taim tomat = new Taim("Tomat",7);
-        peenar.külva(kurk);
-        peenar.külva(tomat);
-        peenar.kasvata(); //kui siin korraga kasvatada siis kõik töötab, järjest ei tööta, ei oska kuidagi ekraani värskendada
+//        Taim kurk = new Taim("Kurk",7);
+//        Taim tomat = new Taim("Tomat",7);
+        groupTaimed = new Group();
+        groupTaust = new Group();
+
+//        peenar.külva(kurk);
+//        peenar.külva(tomat);
+
         List<Taim> taimed = peenar.getTaimed();
 
-        Group group = new Group();
-        Scene scene = new Scene(group, Color.WHITE);
+        Scene scene1 = new Scene(groupTaust, Color.WHITE);
         primaryStage.setScene(scene);
-        primaryStage.show();
 
         Rectangle muld = new Rectangle(1500, 250, Color.BROWN);
-        group.setLayoutX(200);
-        group.setLayoutY(650);
+        groupTaust.setLayoutX(200);
+        groupTaust.setLayoutY(650);
 
         Button külva = new Button("Külva");
         külva.setLayoutX(100);
         külva.setLayoutY(275);
         külva.setOnAction(actionEvent -> {
             //TODO küsida mida külvata
-            System.out.println(peenar.toString());
+            sisendiKontroll(peenar);
         });
         Button kasta = new Button("Kasta");
         kasta.setLayoutX(150);
         kasta.setLayoutY(275);
         kasta.setOnAction(actionEvent -> {
             peenar.kasta();
+
         });
         Button väeta = new Button("Väeta");
         väeta.setLayoutX(200);
         väeta.setLayoutY(275);
-        kasta.setOnAction(actionEvent -> {
+        väeta.setOnAction(actionEvent -> {
             peenar.väeta();
         });
         Button rohi = new Button("Rohi");
         rohi.setLayoutX(250);
         rohi.setLayoutY(275);
-        kasta.setOnAction(actionEvent -> {
+        rohi.setOnAction(actionEvent -> {
             peenar.rohi();
         });
         Button lõika = new Button("Lõika");
@@ -67,32 +76,102 @@ public class Main extends Application{
             peenar.lõika();
         });
 
-        group.getChildren().addAll(muld, külva, kasta, väeta, rohi, lõika);
+        groupTaust.getChildren().addAll(muld, külva, kasta, väeta, rohi, lõika);
+
+        primaryStage.setScene(groupTaust);
+        primaryStage.show();
+    }
+
+    public static void kasvataGraafika(Peenar peenar){
+        List<Taim> taimed = peenar.getTaimed();
+        groupTaimed.getChildren().clear();
 
         for (int i = 0; i < taimed.size(); i++) {
-            System.out.println(taimed.get(i).toString());
+            if (taimed.get(i) == null) continue;
+            //System.out.println(taimed.get(i).toString());
             Line istik = new Line((i + 1) * 100, -25 * (taimed.get(i).getKõrgus()), (i + 1) * 100, 0);
-            group.getChildren().add(istik);
+            groupTaimed.getChildren().add(istik);
             for (int j = 0; j < taimed.get(i).getKõrgus(); j++) { //parempool
                 if (taimed.get(i).getParempool().get(j).get(0)) {
                     Line leht = new Line((i + 1) * 100 + 10, (j + 20) * -j - 10, (i + 1) * 100, (j + 20) * -j);
-                    group.getChildren().add(leht);
+                    groupTaimed.getChildren().add(leht);
                     if (taimed.get(i).getParempool().get(j).get(1)) {
                         Ellipse vili = new Ellipse((i + 1) * 100 + 10, (j + 20) * -j - 10, 2, 2);
-                        group.getChildren().add(vili);
+                        groupTaimed.getChildren().add(vili);
                     }
                 }
             }
             for (int j = 0; j < taimed.get(i).getKõrgus(); j++) { //vasak
                 if (taimed.get(i).getVasakpool().get(j).get(0)) {
                     Line leht = new Line((i + 1) * 100 - 10, (j + 23) * -j - 10, (i + 1) * 100, (j + 23) * -j);
-                    group.getChildren().add(leht);
+                    groupTaimed.getChildren().add(leht);
                     if (taimed.get(i).getVasakpool().get(j).get(1)) {
                         Ellipse vili = new Ellipse((i + 1) * 100 - 10, (j + 23) * -j - 10, 2, 2);
-                        group.getChildren().add(vili);
+                        groupTaimed.getChildren().add(vili);
                     }
                 }
             }
         }
+    }
+
+    /**
+     * Kontrollib kasutajalt saadud sisendit ning sooritab antud sõnale sarnaseima tegevuse
+     */
+    public static void sisendiKontroll(Peenar peenar) {
+        // Loo scanner
+        Scanner sc = new Scanner(System.in);
+        boolean protsess = true;
+        String sarnaseimTaim = "";
+        int taim = 0;
+
+        // Küsi kuni saad sobiva vastuse
+        while (protsess) {
+            System.out.println("Sisesta taim mida soovid istutada: ");
+            String[] vastus = sc.nextLine().toLowerCase().split(" ");
+
+            tagasi: // Label kust loopidest välja saab
+            for (String s : taimeSordid) {
+                int sarnasusi = 0;
+                for (int j = 0; j < s.length(); j++) {
+                    // Kui sõna on piisavalt sarnane antud taimega, murra vali see ja murra välja
+                    if (sarnasusi >= 3) {
+                        sarnaseimTaim = s;
+                        protsess = false;
+                        break tagasi; // Murra kahest loopist välja
+                    }
+
+                    // Kui kasutaja antud sõnas on sarnane täht, lisa sarnasuspunkte
+                    if (vastus[0].contains(s.substring(j, j + 1))) {
+                        sarnasusi++;
+                    }
+                }
+            }
+
+            // Kui sõnal pole piisavalt sarnasusi, küsi uuesti
+            if (sarnaseimTaim.equals("")) {
+                System.out.println("Ei tea soovitud taime. Proovi uuesti ");
+            }
+        }
+
+        System.out.println("Istutan taime: " + sarnaseimTaim);
+        switch(sarnaseimTaim) {
+            case "hernes" :
+                peenar.külva(new Taim("Hernes", 9));
+                break;
+            case "tomat" :
+                peenar.külva(new Taim("Hernes", 4));
+                break;
+            case "kurk" :
+                peenar.külva(new Taim("Hernes", 7));
+                break;
+            case "petersell" :
+                peenar.külva(new Taim("Hernes", 6));
+                break;
+            case "uba" :
+                peenar.külva(new Taim("Hernes", 10));
+                break;
+
+        }
+
     }
 }
--- src/main/java/com/example/pjkt/Peenar.java
+++ src/main/java/com/example/pjkt/Peenar.java
@@ -7,50 +7,74 @@ public class Peenar {
     private int toitained;
     private int niiskus;
     private int umbrohi;
-    private List<Taim> taimed = new ArrayList<>();
+    private List<Taim> taimed;
 
     public Peenar(int toitained, int niiskus, int umbrohi) {
         this.toitained = toitained;
         this.niiskus = niiskus;
         this.umbrohi = umbrohi;
+        this.taimed = new ArrayList<Taim>();
+    }
+
+    public boolean kasKasvab(){
+//        return 2 * Math.random() * (niiskus/100.0) * (toitained/100.0) > 0.3;
+        return true;
     }
 
     public void kasvata(){
-        boolean kasKasvab = true; //TODO
-        if (kasKasvab){
-            for(Taim taim : taimed){
+        if (kasKasvab())
+        {
+            for (Taim taim : taimed) {
                 taim.kasva();
             }
+            Main.kasvataGraafika(this);
+            tarbiToitaineid();
+            kasvataUmbrohtu();
         }
+        kuiva();
+
+
+
     }
 
     public void külva(Taim taim){
-        try {
-            if (taimed.size() < 14) {
-                taimed.add(taim);
-            }
-        } catch (Exception e) {
+        if (taimed.size() < 14) {
             taimed.add(taim);
         }
+        kasvata();
     }
 
+    /**
+     * Kasta peenart
+     */
     public void kasta() {
-        //TODO
+        niiskus = Math.min(niiskus + 10, 100);
         kasvata();
     }
 
+    /**
+     * Väeta peenart
+     */
     public void väeta() {
-        //TODO
+        toitained = Math.min(toitained + 10, 100);
         kasvata();
     }
 
+    /*
+     * Eemalda umbrohtu
+     */
     public void rohi() {
-        //TODO
+        umbrohi = Math.max(0, umbrohi - 10);
         kasvata();
     }
 
+    /**
+     * Korja saak taimelt ning eemalda see potist
+     */
     public void lõika() {
-        //TODO
+        if (taimed.size() == 0) return;
+        //Main.lisaSaak(taim.getNimetus(), taim.getSaak());
+        taimed.remove(taimed.get(0));
         kasvata();
     }
 
@@ -69,6 +93,28 @@ public class Peenar {
     public List<Taim> getTaimed() {
         return taimed;
     }
+
+    /**
+     * Muld kuivab kui seda mitte kasta
+     */
+    public void kuiva() {
+        niiskus = Math.max(0, niiskus - (int)(10 * Math.random()));
+    }
+
+    /**
+     * Taim tarbib toitaineid
+     */
+    public void tarbiToitaineid() {
+        toitained = Math.max(0, toitained - (int)(10 * Math.random()));
+    }
+
+    /**
+     * Kasvata umbrohtu peenrale
+     */
+    public void kasvataUmbrohtu() {
+        umbrohi = Math.min(umbrohi + (int)(5 * Math.random()), 100);
+    }
+
 
     @Override
     public String toString() {
