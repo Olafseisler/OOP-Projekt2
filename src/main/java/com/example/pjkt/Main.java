f5ec16c26e6087038016489197d609d68cabd959
Karl Artur Lokk <46595806+Paarthunax@users.noreply.github.com>
2022-05-19 12:16:46 +0300

    eemaldasin lähima sisendi kontrolli sest see ei tööta nagu peab

--- src/main/java/com/example/pjkt/Main.java
+++ src/main/java/com/example/pjkt/Main.java
@@ -3,8 +3,12 @@ package com.example.pjkt;
 import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.SubScene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.TextInputDialog;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.TilePane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Ellipse;
 import javafx.scene.shape.Line;
@@ -12,12 +16,12 @@ import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
 import java.util.List;
-import java.util.Scanner;
+import java.util.Optional;
 
 public class Main extends Application{
 
-    public static Group groupTaimed;
-    public static Group groupTaust;
+    public static Group group;
+    static Pane pane = new Pane();
     private static String[] taimeSordid = new String[] {"hernes", "tomat", "kurk", "petersell", "uba"};
 
     public static void main(String[] args){
@@ -26,28 +30,21 @@ public class Main extends Application{
 
     public void start(Stage primaryStage) throws Exception {
         Peenar peenar = new Peenar(100,100,0);
-//        Taim kurk = new Taim("Kurk",7);
-//        Taim tomat = new Taim("Tomat",7);
-        groupTaimed = new Group();
-        groupTaust = new Group();
-
-//        peenar.külva(kurk);
-//        peenar.külva(tomat);
+        group = new Group();
 
         List<Taim> taimed = peenar.getTaimed();
 
-        Scene scene1 = new Scene(groupTaust, Color.WHITE);
+        Scene scene = new Scene(group, Color.WHITE);
         primaryStage.setScene(scene);
 
         Rectangle muld = new Rectangle(1500, 250, Color.BROWN);
-        groupTaust.setLayoutX(200);
-        groupTaust.setLayoutY(650);
+        group.setLayoutX(200);
+        group.setLayoutY(650);
 
         Button külva = new Button("Külva");
         külva.setLayoutX(100);
         külva.setLayoutY(275);
         külva.setOnAction(actionEvent -> {
-            //TODO küsida mida külvata
             sisendiKontroll(peenar);
         });
         Button kasta = new Button("Kasta");
@@ -75,103 +72,97 @@ public class Main extends Application{
         lõika.setOnAction(actionEvent -> {
             peenar.lõika();
         });
+        group.getChildren().addAll(muld, külva, kasta, väeta, rohi, lõika,pane);
 
-        groupTaust.getChildren().addAll(muld, külva, kasta, väeta, rohi, lõika);
-
-        primaryStage.setScene(groupTaust);
         primaryStage.show();
     }
 
     public static void kasvataGraafika(Peenar peenar){
         List<Taim> taimed = peenar.getTaimed();
-        groupTaimed.getChildren().clear();
+        pane.getChildren().clear();
 
         for (int i = 0; i < taimed.size(); i++) {
             if (taimed.get(i) == null) continue;
             //System.out.println(taimed.get(i).toString());
             Line istik = new Line((i + 1) * 100, -25 * (taimed.get(i).getKõrgus()), (i + 1) * 100, 0);
-            groupTaimed.getChildren().add(istik);
+            pane.getChildren().add(istik);
             for (int j = 0; j < taimed.get(i).getKõrgus(); j++) { //parempool
                 if (taimed.get(i).getParempool().get(j).get(0)) {
                     Line leht = new Line((i + 1) * 100 + 10, (j + 20) * -j - 10, (i + 1) * 100, (j + 20) * -j);
-                    groupTaimed.getChildren().add(leht);
+                    pane.getChildren().add(leht);
                     if (taimed.get(i).getParempool().get(j).get(1)) {
                         Ellipse vili = new Ellipse((i + 1) * 100 + 10, (j + 20) * -j - 10, 2, 2);
-                        groupTaimed.getChildren().add(vili);
+                        pane.getChildren().add(vili);
                     }
                 }
             }
             for (int j = 0; j < taimed.get(i).getKõrgus(); j++) { //vasak
                 if (taimed.get(i).getVasakpool().get(j).get(0)) {
                     Line leht = new Line((i + 1) * 100 - 10, (j + 23) * -j - 10, (i + 1) * 100, (j + 23) * -j);
-                    groupTaimed.getChildren().add(leht);
+                    pane.getChildren().add(leht);
                     if (taimed.get(i).getVasakpool().get(j).get(1)) {
                         Ellipse vili = new Ellipse((i + 1) * 100 - 10, (j + 23) * -j - 10, 2, 2);
-                        groupTaimed.getChildren().add(vili);
+                        pane.getChildren().add(vili);
                     }
                 }
             }
         }
     }
 
+    public static String küsiSisend(){
+        TilePane tilePane = new TilePane();
+        TextInputDialog textInputDialog = new TextInputDialog("");
+        textInputDialog.setHeaderText("Sisestage soovitud taim" + "\n" + "hernes,tomat,kurk,petersell,uba");
+        textInputDialog.setTitle("Sisend");
+        Optional<String> valik = textInputDialog.showAndWait();
+        group.getChildren().add(tilePane);
+        if (valik.isPresent()){
+            return valik.get();
+        }
+        else return "välju";
+    }
+
     /**
      * Kontrollib kasutajalt saadud sisendit ning sooritab antud sõnale sarnaseima tegevuse
      */
     public static void sisendiKontroll(Peenar peenar) {
-        // Loo scanner
-        Scanner sc = new Scanner(System.in);
-        boolean protsess = true;
-        String sarnaseimTaim = "";
-        int taim = 0;
 
-        // Küsi kuni saad sobiva vastuse
-        while (protsess) {
-            System.out.println("Sisesta taim mida soovid istutada: ");
-            String[] vastus = sc.nextLine().toLowerCase().split(" ");
-
-            tagasi: // Label kust loopidest välja saab
-            for (String s : taimeSordid) {
-                int sarnasusi = 0;
-                for (int j = 0; j < s.length(); j++) {
-                    // Kui sõna on piisavalt sarnane antud taimega, murra vali see ja murra välja
-                    if (sarnasusi >= 3) {
-                        sarnaseimTaim = s;
-                        protsess = false;
-                        break tagasi; // Murra kahest loopist välja
-                    }
-
-                    // Kui kasutaja antud sõnas on sarnane täht, lisa sarnasuspunkte
-                    if (vastus[0].contains(s.substring(j, j + 1))) {
-                        sarnasusi++;
-                    }
+        boolean running = true;
+        while(running) {
+            String vastus = küsiSisend();
+            try {
+                switch (vastus) {
+                    case "välju":
+                        running = false;
+                        break;
+                    case "hernes":
+                        peenar.külva(new Taim("Hernes", 9));
+                        running = false;
+                        break;
+                    case "tomat":
+                        peenar.külva(new Taim("Hernes", 4));
+                        running = false;
+                        break;
+                    case "kurk":
+                        peenar.külva(new Taim("Hernes", 7));
+                        running = false;
+                        break;
+                    case "petersell":
+                        peenar.külva(new Taim("Hernes", 6));
+                        running = false;
+                        break;
+                    case "uba":
+                        peenar.külva(new Taim("Hernes", 10));
+                        running = false;
+                        break;
+                    default:
+                        throw new VigaseNimetuseErind("Tundmatu taim");
                 }
-            }
-
-            // Kui sõnal pole piisavalt sarnasusi, küsi uuesti
-            if (sarnaseimTaim.equals("")) {
-                System.out.println("Ei tea soovitud taime. Proovi uuesti ");
+            } catch (VigaseNimetuseErind e) {
+                Alert erind = new Alert(Alert.AlertType.ERROR);
+                erind.setHeaderText("Tundmatu taim");
+                erind.showAndWait();
             }
         }
-
-        System.out.println("Istutan taime: " + sarnaseimTaim);
-        switch(sarnaseimTaim) {
-            case "hernes" :
-                peenar.külva(new Taim("Hernes", 9));
-                break;
-            case "tomat" :
-                peenar.külva(new Taim("Hernes", 4));
-                break;
-            case "kurk" :
-                peenar.külva(new Taim("Hernes", 7));
-                break;
-            case "petersell" :
-                peenar.külva(new Taim("Hernes", 6));
-                break;
-            case "uba" :
-                peenar.külva(new Taim("Hernes", 10));
-                break;
-
-        }
-
     }
 }
---
+++ src/main/java/com/example/pjkt/VigaseNimetuseErind.java
@@ -0,0 +1,7 @@
+package com.example.pjkt;
+
+public class VigaseNimetuseErind extends RuntimeException{
+    public VigaseNimetuseErind(String message) {
+        super(message);
+    }
+}
